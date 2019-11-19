c02ce6cfd9759eb5115290d3b875906daf3c2c60
Søren Krogh <soren1@hotmail.com>
2019-11-19 17:07:05 +0100

    Updates Example app.

--- Examples/src/main/java/dk/vejdirektoratet/vejdirektoratetsdkexample/MainActivity.kt
+++ Examples/src/main/java/dk/vejdirektoratet/vejdirektoratetsdkexample/MainActivity.kt
@@ -13,6 +13,7 @@ import android.os.Bundle
 
 import dk.vejdirektoratet.vejdirektoratetsdk.*
 import dk.vejdirektoratet.vejdirektoratetsdk.entity.ListEntity
+import dk.vejdirektoratet.vejdirektoratetsdk.entity.MapEntity
 import dk.vejdirektoratet.vejdirektoratetsdk.feed.Feed
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -29,39 +30,44 @@ class MainActivity : AppCompatActivity() {
 
         VejdirektoratetSDK.request(entityTypes = listOf(EntityType.TRAFFIC, EntityType.ROADWORK), region = bounds, viewType = ViewType.LIST, apiKey = apiKey) { result: Feed.Result ->
             when (result) {
-                is Feed.Result.Success -> handleSuccess(result)
-                is Feed.Result.Error -> handleFailure(result)
+                is Feed.Result.Success -> handleSuccess(result, false)
+                is Feed.Result.Error -> handleFailure(result, false)
             }
         }
 
-        /*VejdirektoratetSDK.request(entityTypes = listOf(EntityType.TRAFFIC, EntityType.ROADWORK), region = bounds, viewType = ViewType.LIST, apiKey = "") { result: String ->
-            center_text.text = result
-        }*/
+        VejdirektoratetSDK.request(entityTypes = listOf(EntityType.TRAFFIC, EntityType.ROADWORK), region = bounds, viewType = ViewType.MAP, apiKey = apiKey) { result: Feed.Result ->
+            when (result) {
+                is Feed.Result.Success -> handleSuccess(result, true)
+                is Feed.Result.Error -> handleFailure(result, true)
+            }
+        }
     }
 
-    private fun handleSuccess(result: Feed.Result.Success) {
+    private fun handleSuccess(result: Feed.Result.Success, map: Boolean) {
         if (result.entities.isNotEmpty()) {
             this@MainActivity.runOnUiThread {
-                val header = result.entities.joinToString { entity -> (entity as ListEntity).entityType.value}
-                val description = result.entities.joinToString(separator = "\n") { entity -> (entity as ListEntity).bounds.asLatLngBounds().toString()}
-                updateTexts(header, description)
+                val description = result.entities.joinToString(separator = "\n") { entity -> if (entity is ListEntity) entity.description else (entity as MapEntity).type.value}
+                updateTexts(description, map)
             }
         }
     }
 
-    private fun handleFailure(error: Feed.Result.Error) {
+    private fun handleFailure(error: Feed.Result.Error, map: Boolean) {
         val header: String = when (error) {
             is Feed.Result.HttpError -> error.statusCode.toString()
             else -> "Error!"
         }
 
         this@MainActivity.runOnUiThread {
-            updateTexts(header, error.exception.localizedMessage)
+            updateTexts(error.exception.localizedMessage, map)
         }
     }
 
-    private fun updateTexts(header: String, description: String) {
-        center_text.text = header
-        below_text.text = description
+    private fun updateTexts(content: String, map: Boolean) {
+        if (map) {
+            map_text.text = content
+        } else {
+            list_text.text = content
+        }
     }
 }
